e94803206a06175e38a3d2b86d4300a934811714
Makhmut <mahmudlyder@gmail.com>
2025-10-11 19:10:39 +0500

    refactor: change core project structure according to docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './layout/header/header.component';
-import { NavigationComponent } from './layout/navigation/navigation.component';
-import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { NavigationComponent } from './core/navigation/navigation.component';
+import { FooterComponent } from './core/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
 @Component({
