7fde816331915a8c4e59238774724eec61f74895
Makhmut <mahmudlyder@gmail.com>
2025-10-11 11:38:37 +0500

    feat: add footer links logic

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Link {
+  name: string;
+  url: string;
+}
+interface LinkColumn {
+  title: string;
+  links: Link[];
+}
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  linkColumns: LinkColumn[] = [
+    { title: 'Company', links: [
+        { name: 'About Us', url: '/about' },
+        { name: 'Careers', url: '/careers' },
+      ]
+    },
+  ]
 
 }
