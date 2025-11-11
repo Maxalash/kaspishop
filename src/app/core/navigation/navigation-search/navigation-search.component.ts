b5eb78454f03e4134863b12b3af65170af1c25aa
Makhmut <mahmudlyder@gmail.com>
2025-11-11 20:53:19 +0500

    feat: implement two way binding [(ngModule)]

--- src/app/core/navigation/navigation-search/navigation-search.component.ts
+++ src/app/core/navigation/navigation-search/navigation-search.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon'
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Product } from '../../../market/products/products.component';
 
 
 @Component({
   selector: 'app-navigation-search',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon, FormsModule],
   templateUrl: './navigation-search.component.html',
   styleUrl: './navigation-search.component.css'
 })
 export class NavigationSearchComponent {
   searchResults: Product[] = [];
 
-  requestProducts(name: string){
-    console.log(`Searching products with name: ${name}`);
+  productName: string = '';
+
+  requestProducts(){
+    console.log(`Searching products with name: ${this.productName}`);
   }
 
 }
